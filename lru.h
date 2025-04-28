c3bbb287622dd341ce1975d51ef724c582a6f14e
Elian <d2099080@gmail.com>
2025-04-28 14:53:48 +0800

    Add MAC table implementation with LRU and aging
    - Implement 'lru_hash_add' and 'lru_hash_lookup' based on 'uthash'
    - Manage MAC entries with a doubly linked LRU list
    - Support automatic aging timeout and maximum entry limit
    - Provide 'lru.h' with data structures and function declarations

---
+++ lru.h
@@ -0,0 +1,32 @@
+#include <stdint.h> 
+#include <time.h>
+#include "ut_hash.h"
+
+#define AGING_TIMEOUT 10
+#define LRU_MAX_ENTRIES 5
+
+typedef struct lru_node {
+    struct lru_node *prev;
+    struct lru_node *next;
+} lru_node_t;
+
+typedef struct lru_mac_entry {
+    uint8_t mac[6];
+    int out_port;
+    time_t last_seen;
+    lru_node_t lru;
+    UT_hash_handle hh;
+} lru_mac_entry_t;
+
+void lru_move_to_front(lru_mac_entry_t *entry);
+
+void lru_insert(lru_mac_entry_t *entry);
+
+void lru_remove_tail();
+
+void aging_cleanup(); 
+
+void lru_hash_add(uint8_t *mac, int port);
+
+int lru_hash_lookup(uint8_t *mac);
+
